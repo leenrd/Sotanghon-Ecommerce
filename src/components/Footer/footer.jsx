ac4e1dbe0bd56c21e869a45b7fb28dbba2f48a64
Leenard <lolleenard0@gmail.com>
2023-11-07 20:15:44 +0800

    redone footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,35 +1,33 @@
-import './style-footer.css'
-import { useContext } from 'react'
-import { Link } from "react-router-dom"
-import { CartContext } from '../../App'
+import "./style-footer.css";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "../../App";
 
 function Footer() {
-    const { handleCart } = useContext(CartContext)
+  const { handleCart } = useContext(CartContext);
 
   return (
     <div className="footer">
-        <div className="grid">
-            <div className="footText">
-                <h1>Baked At Midnight </h1>
-                <span className='circle'></span>
-            </div>
-            <div className="links">
-                <div className="menu">
-                    <Link to='/items'>Items</Link>
-                    <Link to='/about'>About</Link>
-                    <Link to='/location'>Locations</Link>
-                    <span
-                    onClick={() => handleCart()}
-                    >Cart</span>
-                </div>
-                <div className="contacts">
-                    <span>09783782898</span>
-                    <span className='emailSotanghon'>sotanghon.hello.com</span>
-                </div>
-            </div>
+      <div className="grid">
+        <div className="footText">
+          <h1>Leenard Zarate </h1>
+          <span className="circle"></span>
         </div>
+        <div className="links">
+          <div className="menu">
+            <Link to="/items">Items</Link>
+            <Link to="/about">About</Link>
+            <Link to="/location">Locations</Link>
+            <span onClick={() => handleCart()}>Cart</span>
+          </div>
+          <div className="contacts">
+            <span>09783782898</span>
+            <span className="emailSotanghon">sotanghon.hello.com</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
